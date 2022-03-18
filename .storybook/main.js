a5dbde83990e547f082d8efae063b87a5046e06a
shzawa <ozawa.shotaro0114@gmail.com>
2022-03-18 10:27:54 +0900

    update: Next.jsでStorybookを使用する為の最低限の設定追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,39 @@
+const path = require('path')
+
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
+  typescript : { reactDocgen: false },
+  stories: ['../src/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
   staticDirs: ['../public'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              auto: true,
+            },
+          },
+        },
+        'sass-loader',
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+    // Fixes npm packages that depend on some modules
+    config.resolve = {
+      extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
+      fallback: {
+        fs: false,
+        path: false,
+      },
+    };
+    // Important: return the modified config
+    return config
+  },
+  core: {
+    builder: 'webpack5',
+  },
 }
