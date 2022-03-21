14d0f7a5277d0482324d8e4dcf477bd96670df32
shzawa <ozawa.shotaro0114@gmail.com>
2022-03-21 10:07:41 +0900

    update: atoms/Icon内をfactoryで共通化

--- src/atoms/Icon/Index.tsx
+++ src/atoms/Icon/Index.tsx
@@ -1,22 +1,49 @@
 import NextImage, { ImageProps } from 'next/image'
 import styles from './styles.module.scss'
 
-export const TrashCanIcon: React.VFC<Omit<ImageProps, 'src'>> = ({
+type IconPresenterProps = Omit<IconContainerProps, 'presenter'>
+export const IconPresenter: React.VFC<IconPresenterProps> = ({
+  iconName,
   height = 20,
   width = 20,
-  className = '',
+  ...props
+}) => (
+  <NextImage
+    src={`/icons/${iconName}.svg`}
+    width={width}
+    height={height}
+    {...props}
+  />
+)
+
+type IconContainerProps = IconProps & {
+  presenter: React.VFC<IconPresenterProps>
+  iconName: string
+}
+export const IconContainer: React.VFC<IconContainerProps> = ({
+  presenter,
   onClick,
+  className = '',
   ...props
 }) => {
   if (onClick) className += ` ${styles.clickable}`
+  return presenter({ onClick, className, ...props })
+}
+
+type IconProps = Omit<ImageProps, 'src'>
+export const iconFactory: (iconName: string) => (props: IconProps) => JSX.Element = (iconName) => function Icon({onClick, ...props}) {
   return (
-    <NextImage
-      src={'/icons/trash-can.svg'}
-      width={width}
-      height={height}
-      className={className}
-      onClick={onClick}
-      {...props}
+    <IconContainer
+      presenter={presenterProps => (
+          <IconPresenter {...presenterProps} />
+      )}
+      {...{ onClick, iconName, props }}
     />
   )
+
 }
+
+export const TrashCanIcon = iconFactory('trash-can')
+export const ChevronRightIcon = iconFactory('chevron-right')
+export const SearchIcon = iconFactory('search')
+export const SettingsIcon = iconFactory('settings')
